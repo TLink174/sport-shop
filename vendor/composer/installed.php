43b557f32322efcee17096cff76fe1c455316d6a
TLink174 <linh174ht@gmail.com>
2022-12-15 00:07:46 +0700

    up_anh

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => 'daf2efa6c784b6ddaabe4c0cf4dc3f6a7a7c999a',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '37088c07e668bf2b2f6021990ccbd2d94542bc15',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -383,9 +383,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => 'daf2efa6c784b6ddaabe4c0cf4dc3f6a7a7c999a',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '37088c07e668bf2b2f6021990ccbd2d94542bc15',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -727,8 +727,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '1.0.0 || 2.0.0 || 3.0.0',
+                0 => '1.0.0 || 2.0.0 || 3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
